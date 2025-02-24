97aba220957b3266aa626ae5621acb52aec5e279
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-02-24 12:07:36 -0700

    moved env to backend

--- server.js
+++ server.js
@@ -17,6 +17,17 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 //
 // MongoDB Connection
+app.get("/api/config", (req, res) => {
+  res.json({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  });
+});
 const uploadRoutes = require("./routes/uploadRoutes"); // Import upload routes
 const MONGO_URI =
   process.env.MONGO_URI || "mongodb://localhost:27017/defaultdb";
